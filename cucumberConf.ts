93ed98870fe1915ef2a8e835cf3ed0d0e939eaf2
ImRaNM-001 <imu163@gmail.com>
2020-04-09 14:08:01 +0530

    commit all

--- cucumberConf.ts
+++ cucumberConf.ts
@@ -2,28 +2,34 @@
 Basic configuration to run your cucumber
 feature files and step definitions with protractor.
 **/
-exports.config = {
 
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+// import {browser, Config} from "protractor";
+// import * as path from "path";
+
+export const config: Config = {
+    // seleniumAddress: 'http://localhost:4444/wd/hub',
     directConnect: true,
+    getPageTimeout: 50000,
+    allScriptsTimeout: 50000,
   
-    baseUrl: 'https://angularjs.org/',
-  
+    params: {
+    baseUrl: 'https://www.google.com/ncr'
+    }
+
     capabilities: {
         browserName:'chrome'
     },
   
-    framework: 'custom',  // set to "custom" instead of cucumber.
-  
+    framework: 'custom',  // set to "custom" instead of cucumber.  
     frameworkPath: require.resolve('protractor-cucumber-framework'),  // path relative to the current config file
   
-    specs: ['./specs/cucumber/*.feature'],     // Specs here are the cucumber feature files
-    
+    specs: ['./specs/cucumber/featureFiles/*.feature'],     // specs here are the cucumber feature files    
   
     // cucumber command line options
     cucumberOpts: {
-      require: ['./specs/cucumber/*.ts'],  // require step definition files before executing features
-      tags: [],                      // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+      require: ['./specs/cucumber/stepDefinition.ts'],  // require step definition files before executing features
+      tags: false,
+    // tags: [],                      // <string[]> (expression) only execute the features or scenarios with tags matching the expression
       strict: true,                  // <boolean> fail if there are any undefined or pending steps
       format: ["pretty"],            // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
       'dry-run': false,              // <boolean> invoke formatters without executing steps
@@ -32,5 +38,6 @@ exports.config = {
   
    onPrepare: () => {
       browser.manage().window().maximize(); // maximize the browser before executing the feature files
+      browser.ignoreSynchronization = true;  
     }
 };
---
+++ specs/cucumber/featureFiles/1stTest.feature
@@ -0,0 +1,29 @@
+Feature: Write test case with cucumber and protractor using typescript.
+
+# 1st scenario
+Scenario: Launch browser, hit gmail url and verify page title
+Given Open the google homePage
+When Hit the gmail urlLink
+Then Verify the page title of gmail loginPage
+
+# 2nd scenario
+Scenario: Caluclate sum of a given array
+Then Verify sum of all elements of the array 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
